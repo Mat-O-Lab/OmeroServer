93cf2058c827401958c76ec63bf4367f1672bc44
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-02-23 08:04:25 +0100

    changed rights of public group to default config of read-only, renamed network

--- 91-add-public-user.sh
+++ 91-add-public-user.sh
@@ -10,7 +10,7 @@ if [ "$CONFIG_omero_web_public_enable" = "True" ]; then
     #$omero config set omero.web.server_list "[[\"$OMEROHOST\", 4064, \"omero\"]]"
     echo "adding public user"
     $omero login -s $OMEROHOST -p 4064 -u root -w $ROOTPASS || true
-    $omero group add public --perms='r-----' || true
+    $omero group add public --perms='rwr---' || true
     $omero user add $CONFIG_omero_web_public_user Public User --group-name public -P $CONFIG_omero_web_public_password || true
     echo "successfully added public user"
 fi
